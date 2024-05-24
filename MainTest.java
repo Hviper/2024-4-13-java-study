f805d7044429b7b39ae6374230af726e82eaa6a8
hdc <hdc1467520438@gmail.com>
2024-05-24 10:55:13 +0800

    第一次提交_dev_branch01_01_的分支继续分支，然后第一次提交

---
+++ MainTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author 拾光
+ * @version 1.0
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+    }
+}
